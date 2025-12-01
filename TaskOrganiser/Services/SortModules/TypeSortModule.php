82919aa5b31c704ce7e9cfb126a5642600dc8ae9
Kristian Skov <kris701kj@gmail.com>
2025-12-01 09:39:38 +0100

    Create a sort module to sort by Type

---
+++ TaskOrganiser/Services/SortModules/TypeSortModule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Leantime\Plugins\TaskOrganiser\Services\SortModules;
+
+use Leantime\Plugins\TaskOrganiser\Services\SortModules\BaseSortModule;
+use Leantime\Domain\Tickets\Models\Tickets as TicketModel;
+
+class TypeSortModule extends BaseSortModule
+{
+    public array $map = [];
+
+    public function __construct(
+        $data
+    ) {
+        $this->map = get_object_vars($data->map);
+    }
+
+    public function Calculate(TicketModel $ticket) : int{
+        if (array_key_exists($ticket->type, $this->map)){
+            $value = $this->map[$ticket->type];
+            if ($value != null){
+                return $value;
+            }
+        }
+        return 0;
+    }
+}
